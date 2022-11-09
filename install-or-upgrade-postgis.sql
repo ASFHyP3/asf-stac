a09131a017c2d901f51c903610837176587782d3
Jake Herrmann <jtherrmann@alaska.edu>
2022-11-09 10:13:36 -0900

    change postgis version to test script failure

--- install-or-upgrade-postgis.sql
+++ install-or-upgrade-postgis.sql
@@ -1,5 +1,5 @@
 \set ON_ERROR_STOP TRUE
-\set postgis_version '3.1.5'
+\set postgis_version '3.1.4'
 
 CREATE EXTENSION IF NOT EXISTS postgis WITH VERSION :'postgis_version';
 ALTER EXTENSION postgis UPDATE TO :'postgis_version';
