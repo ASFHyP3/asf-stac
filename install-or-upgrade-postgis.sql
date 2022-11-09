f8499e633a8f4dfabea01d9444ff42300f0a3668
Jake Herrmann <jtherrmann@alaska.edu>
2022-11-09 09:56:35 -0900

    fix postgis install script

--- install-or-upgrade-postgis.sql
+++ install-or-upgrade-postgis.sql
@@ -1,3 +1,5 @@
-postgis_version CONSTANT string := '3.1.5';
-CREATE EXTENSION IF NOT EXISTS postgis WITH VERSION postgis_version;
-ALTER EXTENSION postgis UPDATE TO postgis_version;
+\set ON_ERROR_STOP TRUE
+\set postgis_version '3.1.5'
+
+CREATE EXTENSION IF NOT EXISTS postgis WITH VERSION :'postgis_version';
+ALTER EXTENSION postgis UPDATE TO :'postgis_version';
