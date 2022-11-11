970b977b4c87e8fd6a724ce133e5249a45181e65
Jake Herrmann <jtherrmann@alaska.edu>
2022-11-10 15:58:36 -0900

    fix database perms bug

--- configure-database-roles.sql
+++ configure-database-roles.sql
@@ -2,3 +2,6 @@
 
 ALTER ROLE pgstac_ingest LOGIN PASSWORD :'db_ingest_password';
 ALTER ROLE pgstac_read LOGIN PASSWORD :'db_read_password';
+
+-- TODO this will no longer be needed after the pgstac_read permissions bug is fixed: https://github.com/stac-utils/pgstac/issues/146
+GRANT SELECT ON pgstac.collections TO pgstac_read;
