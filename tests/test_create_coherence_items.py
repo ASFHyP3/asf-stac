0efd0d00e9669643da495bcd7bcecef80ebe4600
Jake Herrmann <jtherrmann@alaska.edu>
2022-11-29 16:04:26 -0900

    add test_parse_s3_key

--- tests/test_create_coherence_items.py
+++ tests/test_create_coherence_items.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from shapely import geometry
 
@@ -22,6 +22,44 @@ def test_season_datetime_averages():
     assert SEASON_DATETIME_AVERAGES['FALL'] - SEASON_DATE_RANGES['FALL'][0] \
            == SEASON_DATE_RANGES['FALL'][1] - SEASON_DATETIME_AVERAGES['FALL'] \
            == timedelta(days=45)
+
+
+def test_parse_s3_key():
+    assert create_coherence_items.parse_s3_key('data/tiles/N00E005/N00E005_124D_inc.tif') \
+        == create_coherence_items.ItemMetadata(
+            id='N00E005_124D_inc',
+            bbox=geometry.box(5, -1, 6, 0),
+            tileid='N00E005',
+            product='INC',
+        )
+
+    assert create_coherence_items.parse_s3_key('data/tiles/N00E005/N00E005_fall_vh_AMP.tif') \
+        == create_coherence_items.ItemMetadata(
+            id='N00E005_fall_vh_AMP',
+            bbox=geometry.box(5, -1, 6, 0),
+            tileid='N00E005',
+            product='AMP',
+            extra=create_coherence_items.ExtraItemMetadata(
+                season='FALL',
+                date_range=(datetime(2020, 9, 1), datetime(2020, 11, 30)),
+                datetime=datetime(2020, 10, 16, 0),
+                polarization='VH',
+            ),
+        )
+
+    assert create_coherence_items.parse_s3_key('data/tiles/N00E011/N00E011_winter_vv_COH36.tif') \
+        == create_coherence_items.ItemMetadata(
+            id='N00E011_winter_vv_COH36',
+            bbox=geometry.box(11, -1, 12, 0),
+            tileid='N00E011',
+            product='COH36',
+            extra=create_coherence_items.ExtraItemMetadata(
+                season='WINTER',
+                date_range=(datetime(2019, 12, 1), datetime(2020, 2, 28)),
+                datetime=datetime(2020, 1, 14, 12),
+                polarization='VV',
+            ),
+        )
 
 
 def test_item_id_from_s3_key():
