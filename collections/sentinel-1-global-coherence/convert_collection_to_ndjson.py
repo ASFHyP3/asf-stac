744c9c703f02dea34ae41f781c1900a7bf831180
Jake Herrmann <jtherrmann@alaska.edu>
2022-12-01 17:03:14 -0900

    add newline to ndjson

--- collections/sentinel-1-global-coherence/convert_collection_to_ndjson.py
+++ collections/sentinel-1-global-coherence/convert_collection_to_ndjson.py
@@ -6,7 +6,7 @@ parser.add_argument('collection', type=Path)
 args = parser.parse_args()
 
 with args.collection.open() as f:
-    data = f.read().replace('\n', '')
+    data = f.read().replace('\n', '') + '\n'
 
 with args.collection.with_suffix('.ndjson').open('w') as f:
     f.write(data)
