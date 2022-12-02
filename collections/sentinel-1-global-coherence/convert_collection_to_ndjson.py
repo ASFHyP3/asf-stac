28f12eca62137c246f8e51fe9ebac96f6559a7d1
Jake Herrmann <jtherrmann@alaska.edu>
2022-12-01 17:00:58 -0900

    Ingest coherence collection JSON on deployment

---
+++ collections/sentinel-1-global-coherence/convert_collection_to_ndjson.py
@@ -0,0 +1,12 @@
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('collection', type=Path)
+args = parser.parse_args()
+
+with args.collection.open() as f:
+    data = f.read().replace('\n', '')
+
+with args.collection.with_suffix('.ndjson').open('w') as f:
+    f.write(data)
