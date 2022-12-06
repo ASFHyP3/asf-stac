e0cbf6f981d182abe59fb9f3e81734705aeeb74a
Andrew Johnston <asjohnston@alaska.edu>
2022-12-05 15:01:24 -0900

    revise convert_collections_to_ndjson.py to support multiple collections

--- convert_collections_to_ndjson.py
+++ convert_collections_to_ndjson.py
@@ -3,11 +3,13 @@ import json
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument('collection', type=Path)
+parser.add_argument('--output-file', type=Path, default='collections.ndjson')
+parser.add_argument('collections', type=Path, nargs='+')
 args = parser.parse_args()
 
-with args.collection.open() as f:
-    data = json.load(f)
-
-with args.collection.with_suffix('.ndjson').open('w') as f:
-    json.dump(data, f)
+with args.output_file.open('w') as output_file:
+    for collection in args.collections:
+        with collection.open() as f:
+            data = json.load(f)
+        json.dump(data, output_file)
+        output_file.write('\n')
