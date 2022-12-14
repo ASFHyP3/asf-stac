4a34bbf1c625650975c0b2e0be7bbf39141592a5
jiangzhu <jzhu4@alaska.edu>
2022-12-14 11:29:24 -0900

    improve the code style

--- tests/test_create_hand_items.py
+++ tests/test_create_hand_items.py
@@ -30,31 +30,31 @@ def test_create_stac_item():
                              [5.9998611, 1.0001389]]],
         }},
     ) == {
-           'type': 'Feature',
-           'stac_version': '1.0.0',
-           'id': 'Copernicus_DSM_COG_10_N00_00_E006_00_HAND',
-           'properties': {
-               'datetime': None,
-               'start_datetime': datetime(2010, 12, 1, tzinfo=timezone.utc),
-               'end_datetime': datetime(2015, 2, 1, tzinfo=timezone.utc),
-           },
-           'geometry': {
-               'type': 'Polygon',
-               'coordinates': [[[5.9998611, 1.0001389],
-                                [5.9998611, 0.0001389],
-                                [6.9998611, 0.0001389],
-                                [6.9998611, 1.0001389],
-                                [5.9998611, 1.0001389]]],
-           },
-           'assets': {
-               'data': {
-                   'href': 'foo.com/v1/2021/Copernicus_DSM_COG_10_N00_00_E006_00_HAND.tif',
-                   'type': 'image/tiff; application=geotiff',
-               },
-           },
-           'bbox': (5.9998611, 0.0001389, 6.9998611, 1.0001389),
-           'stac_extensions': [],
-           'collection': create_hand_items.COLLECTION_ID,
+            'type': 'Feature',
+            'stac_version': '1.0.0',
+            'id': 'Copernicus_DSM_COG_10_N00_00_E006_00_HAND',
+            'properties': {
+                'datetime': None,
+                'start_datetime': datetime(2010, 12, 1, tzinfo=timezone.utc),
+                'end_datetime': datetime(2015, 2, 1, tzinfo=timezone.utc),
+            },
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[5.9998611, 1.0001389],
+                                 [5.9998611, 0.0001389],
+                                 [6.9998611, 0.0001389],
+                                 [6.9998611, 1.0001389],
+                                 [5.9998611, 1.0001389]]],
+            },
+            'assets': {
+                'data': {
+                    'href': 'foo.com/v1/2021/Copernicus_DSM_COG_10_N00_00_E006_00_HAND.tif',
+                    'type': 'image/tiff; application=geotiff',
+                },
+            },
+            'bbox': (5.9998611, 0.0001389, 6.9998611, 1.0001389),
+            'stac_extensions': [],
+            'collection': create_hand_items.COLLECTION_ID,
             'links': [
                 {
                     'href': 'https://copernicus-dem-30m.s3.eu-central-1.amazonaws.com/'
